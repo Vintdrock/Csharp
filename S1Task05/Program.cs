f6d4132eb02a19d8abd8ce8eb6f0af5b0cfda20d
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-06 15:30:45 +0300

    Решение задачи Task05

--- S1Task05/Program.cs
+++ S1Task05/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number *= -1;
+if (number >= 100 && number <= 999)
+
+
+
+{
+    int lastDigit = number % 10;
+    Console.Write($"Последняя цифра числа: {lastDigit}");
+}
+
+else
+{
+    Console.Write("Неверное число!");
+}
