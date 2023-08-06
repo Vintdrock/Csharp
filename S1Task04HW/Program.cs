ac2a9dadd1b1ae9cfdf6ddb0b80dfaaf7331208c
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-06 20:43:30 +0300

    Решение домашней задачи Task04

--- S1Task04HW/Program.cs
+++ S1Task04HW/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое целое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье целое число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    if(firstNumber > thirdNumber)
+    {
+        Console.WriteLine($"max = {firstNumber}");
+    }
+    else
+    {
+        Console.WriteLine($"max = {thirdNumber}");
+    }
+    
+}
+
+else
+{
+    if(secondNumber > thirdNumber)
+    {
+        Console.WriteLine($"max = {secondNumber}");
+    }
+    else
+    {
+        Console.WriteLine($"max = {thirdNumber}");
+    }
+}
