328e926482ac06efa0396c444aa9f6554c50ef5d
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-11 17:19:39 +0300

    Решение задачи Task14

--- S2Task14/Program.cs
+++ S2Task14/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+bool MultiplicityTwoNumbers(int num, int num2, int num3)
+{
+    return num % num1 == 0 && num % num2 == 0; 
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = MultiplicityTwoNumbers(num, 7, 23);
+
+Conssole.WriteLine(result ? "Кратно" : "Не кратно");
+
+
+// bool MultiplicityTwoNumbers(int num, int num1, int num2)
+// {
+//     return num % num1 == 0 && num % num2 == 0;
+// }
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// bool result = MultiplicityTwoNumbers(number, 3, 11);
+
+// Console.WriteLine(result? "Кратно" : "Не кратно");
