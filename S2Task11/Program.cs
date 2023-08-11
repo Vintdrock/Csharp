a2ea27227b1d741201af8decbd36a73526664fd6
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-11 17:13:16 +0300

    Решение задачи Task11

--- S2Task11/Program.cs
+++ S2Task11/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого
+// числа. 456 -> 46 782 -> 72 918 -> 98
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);
+
+int firstDigit = number / 100;
+int secondDigit = number % 10;
+int result = firstDigit * 10 + secondDigit;
+Console.WriteLine($"Случайное число {number} c удаленной второй цифрой = {result}");
