3515602d1d0f4d00e92bc333e825e00ec469b337
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-11 18:42:45 +0300

    Добавлено решение домашней задачи Task13

--- S2Task13HW/Program.cs
+++ S2Task13HW/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <100) {
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+Console.WriteLine(FindThreeDigit(number) % 10);
+
+//Функция
+int FindThreeDigit(int num){
+    if (num > 99 && num < 1000) {
+    num = num % 10;
+    }
+else 
+    while (num > 1000) {
+    num = num / 10;
+    }
+    return num;
+}
