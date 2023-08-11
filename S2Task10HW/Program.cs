2325850205b1c12d3644973d0bf8b0e3a90b7e62
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-11 17:41:24 +0300

    Добавлено решение домашней задачи Task10

--- S2Task10HW/Program.cs
+++ S2Task10HW/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+bool CheckThreeDigits(int threeDigit)
+{
+    return threeDigit > 99 && threeDigit < 1000;
+}
+
+
+int SecondDigit(int num)
+{
+    int secondDigit = (num / 10) % 10;
+    return secondDigit;
+}
+
+Console.Write("Введите  трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (CheckThreeDigits(number) == true)
+{
+    Console.WriteLine(SecondDigit(number));
+}
+else Console.WriteLine("Вы ввели НЕ трехзначное число!");
