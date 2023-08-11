0e9a02db581babc16f3440d31bb68c220317d917
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-11 17:17:00 +0300

    Решение задачи Task12

--- S2Task12/Program.cs
+++ S2Task12/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если первое число не кратно
+// второму, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите первое число");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+int MultiplicityTwoNumbers(int first, int second)
+{
+    return first % second;
+}
+int remainder = MultiplicityTwoNumbers(firstDigit, secondDigit);
+if (remainder == 0)
+    Console.WriteLine("Кратно");
+else
+    Console.WriteLine($"Не кратно, остаток: {remainder}");
+
+
+//Тернарный оператор
+
+Console.WriteLine(remainder == 0 ? "Кратно" : $"Не кратно, остаток: {remainder}");
