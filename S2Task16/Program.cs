deec607a205b86a4d807fc5da93bd302a86ed3b1
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-11 17:28:00 +0300

    Решение задачи Task16

--- S2Task16/Program.cs
+++ S2Task16/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+
+bool MultiplicityTwoNumbers (int num, int num2)
+{
+    return num * num == num2 || num2 * num2 == num; 
+}
+
+Console.Write("Введите два числа: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+bool result = MultiplicityTwoNumbers(numberOne, numberTwo);
+
+Console.WriteLine(result ? "Является" : "Не является");
+
