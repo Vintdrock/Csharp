782d2ec0ac7432f963cec0ea9f1db634aacaa498
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-11 18:59:48 +0300

    Добавлено решение домашней задачи Task15

--- S2Task15HW/Program.cs
+++ S2Task15HW/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 0 && day < 8) {
+    if (day > 5 && day <8) Console.WriteLine("День недели является выходным");
+    else Console.WriteLine("День недели не является выходным");
+}
+else Console.WriteLine("Цифра не обозначает день недели");
