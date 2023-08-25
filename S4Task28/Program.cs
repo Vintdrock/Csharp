ba0cffaf47c49065659825103db5a902e15de62c
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-25 21:32:13 +0300

    Решение задачи Task 28

--- S4Task28/Program.cs
+++ S4Task28/Program.cs
@@ -4,3 +4,28 @@
 // 4 -> 24
 // 5 -> 120
 
+int Factorial(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            mult *= i; // sum = sum + i
+        }
+    }
+    return mult;
+}
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Factorial(number);
+
+if(number < 1)
+{
+    Console.WriteLine("Введено некорректное число");
+    return;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {number} = {factorial}");
