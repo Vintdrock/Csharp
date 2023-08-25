4ef15434b5577697856f0a798bc39354812919c7
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-25 18:49:03 +0300

    Решение задачи Task22

--- S3Task22/Program.cs
+++ S3Task22/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while(i <= n)
+{
+    Console.WriteLine($"{i, 3} -> {i*i, 5}");
+    i++;
+}
+
+
+void WriteTableSquare(int number)
+{
+    int i = 1;
+    while(i <= number)
+    {
+        //if(i > 3) return;
+        Console.WriteLine($"{i, 3} -> {i*i, 5}");
+        i++;
+    }
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+WriteTableSquare(n);
