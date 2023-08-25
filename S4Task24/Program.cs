91bc65a4a0c7484f684e00734f473e585edc7d4c
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-25 20:47:20 +0300

    Решение задачи Task 24

--- S4Task24/Program.cs
+++ S4Task24/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            sum += i; // sum = sum + i
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+
+if(number < 1)
+{
+    Console.WriteLine("Введено некорректное число");
+    return;
+}
+
+Console.WriteLine($"Сумму чисел от 1 до {number} = {sumNumbers}");
