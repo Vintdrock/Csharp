a7d77e7ac9d913235e5972928d3251c2cfc1a24e
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-25 22:07:58 +0300

    Решение задачи Task 30

--- S4Task30/Program.cs
+++ S4Task30/Program.cs
@@ -3,3 +3,27 @@
 // нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
+void FillArr(int[] mass)
+{
+    int i = 0;
+    var random = new Random();
+    while(i < mass.Length)
+    {
+        mass[i] = random.Next(2);
+        i++;
+    }
+}
+
+void PrintArr(int[] mass)
+{
+    int i = 0;
+    while(i < mass.Length)
+    {
+        Console.Write($"{mass[i]} ");
+        i++;
+    }
+}
+
+int[] array = new int[8];
+FillArr(array);
+PrintArr(array);
