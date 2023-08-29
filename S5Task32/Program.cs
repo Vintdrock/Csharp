1538cd42d4d864f94f9cdc657c953d384bf42732
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-29 21:01:08 +0300

    Решение задачи Task32

--- S5Task32/Program.cs
+++ S5Task32/Program.cs
@@ -2,3 +2,40 @@
 // массива: положительные элементы замените на
 // соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void InversArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] array = CreateArrayRndInt(4, -8, 8);
+PrintArray(array);
+Console.WriteLine();
+InversArray(array);
+PrintArray(array);
