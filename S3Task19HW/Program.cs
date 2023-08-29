57b1d6a54e67cab0d76f327a74d0332f36b84275
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-29 21:07:20 +0300

    Инициализация домашней задачи Task 19

---
+++ S3Task19HW/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 9999 && number < 100000) {
+    if (number/10000 == number%10 && (number/1000)%10 == (number%100)/10) Console.WriteLine("True");
+    else Console.WriteLine("False");
+}
+else {
+    Console.WriteLine("Число не пятизначное");
+    Console.WriteLine("False");
+}
+
+
+// if (number > 9999 && number < 100000) {
+//     return number/10000 == number%10 && (number/1000)%10 == (number%100)/10;
+//       }
+//       else
+//       {
+//         Console.WriteLine("Число не пятизначное");
+//         return false;
+//       }
