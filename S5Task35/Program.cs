843d2749645b09fea502d42843c84eff35b0f4a5
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-29 21:39:26 +0300

    Решение задачи Task 35

--- S5Task35/Program.cs
+++ S5Task35/Program.cs
@@ -7,3 +7,45 @@
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int RangeDigitElementCount(int[] arr, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] >= min && arr[i] <= max ) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите максиамальное и минимальное число для диапазона поиска: ");
+int minimum = Convert.ToInt32(Console.ReadLine());
+int maximum = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArrayRndInt(123, -100, 100);
+PrintArray(array);
+Console.WriteLine();
+int result = RangeDigitElementCount(array, minimum, maximum);
+Console.Write($"В заданном диапазоне находится {result} чисел(-о, -а).");
