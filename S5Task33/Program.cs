b9691b6ffbd2083782275fd43545d7c58befe916
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-29 21:05:53 +0300

    Решение задачи Task33

--- S5Task33/Program.cs
+++ S5Task33/Program.cs
@@ -2,3 +2,41 @@
 // присутствует ли заданное число в массиве. 
 // 4; массив [6, 7, 19, 345, 3] -> нет 3; массив [6, 7, 19, 345, 3] -> да
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+bool FindNumber(int[] arr, int nmb)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] == nmb) return true;
+    }
+    return false;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] array = CreateArrayRndInt(5, -6, 6);
+int number = 5;
+PrintArray(array);
+Console.Write(" -> ");
+bool answer = FindNumber(array, number);
+Console.WriteLine(answer);
