6cdf5da776d234d57f1715e51bca1e3d2983911f
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-31 21:12:32 +0300

    Добавлено решение домашней задачи 27

--- S4Task27HW/Program.cs
+++ S4Task27HW/Program.cs
@@ -3,3 +3,17 @@
 // 82 -> 10
 // 9012 -> 12
 
+int SumNumbers(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма цифр в числе {number} = {sumNumbers}"); 
