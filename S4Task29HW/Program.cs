f46f54b6237ad12cd0d9af272b545f5773efaaeb
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-31 22:41:05 +0300

    Добавлено решение домашней задачи 29

--- S4Task29HW/Program.cs
+++ S4Task29HW/Program.cs
@@ -2,3 +2,35 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+Console.Write("Введите размер массива: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальный элемент массива: ");
+int minNumArr = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальный элемент массива: ");
+int maxNumArr = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayRndInt(sizeArr, minNumArr, maxNumArr);
+Console.WriteLine();
+PrintArray(array);
