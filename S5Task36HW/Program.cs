0630a13f16fcf6bcc735b23e7f892a82e60befc0
Виталий Матюшенко <kuyask@yandex.ru>
2023-08-31 22:01:37 +0300

    Добавлено решение домашней задачи 36

--- S5Task36HW/Program.cs
+++ S5Task36HW/Program.cs
@@ -2,3 +2,44 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int SumNotEvwnArrayNumbers (int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+Console.Write("Введите размер массива: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayRndInt(sizeArr, -100, 100);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+int sumNotEvwnArrayNumbers = SumNotEvwnArrayNumbers (array);
+Console.Write($"Сумма элеменот массива на нечетных позищиях = {sumNotEvwnArrayNumbers}");
