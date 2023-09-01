eb26579bcfb2201145e5cefee3f3f93c3bf1541e
Виталий Матюшенко <kuyask@yandex.ru>
2023-09-01 21:30:27 +0300

    Добавлено решение задачи 42

--- S6Task42/Program.cs
+++ S6Task42/Program.cs
@@ -3,3 +3,18 @@
 // 46 -> 101110
 // 13 -> 1101
 // 2 -> 10
+
+int DecimalToBinary(int number)
+{
+    int binary = 0;
+    int mult = 1;
+    while (number != 0)
+    {
+        binary += mult * (number % 2); 
+        number = number / 2;
+        mult *= 10;
+    }
+    return binary;
+}
+int binar = DecimalToBinary(2);
+Console.WriteLine(binar);
