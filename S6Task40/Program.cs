3e39279a5ac4dc8805d81aa1a4eb7d24cd40cb0a
Виталий Матюшенко <kuyask@yandex.ru>
2023-09-06 21:24:26 +0300

    Добавлено решение задачи 40

---
+++ S6Task40/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник со сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 46 -> 101110
+// 13 -> 1101
+// 2 -> 10
+
+
+bool ExistenceOfTriangle(int firstSide, int secondSide, int thirdSide)
+{
+    if (firstSide + secondSide > thirdSide
+        && firstSide + thirdSide > secondSide
+        && secondSide + thirdSide > firstSide)
+        return true;
+    else return false;
+}
+
+Console.Write("Введите первую сторону: ");
+int side1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторую сторону: ");
+int side2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третью сторону: ");
+int side3 = Convert.ToInt32(Console.ReadLine());
+bool result = ExistenceOfTriangle(side1, side2, side3);
+Console.WriteLine(result ? "существует" : "не существует");
