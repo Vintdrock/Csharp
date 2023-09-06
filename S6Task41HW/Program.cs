7ee13cce4b5c558d9cf18e012b33aba094721ccc
Виталий Матюшенко <kuyask@yandex.ru>
2023-09-06 21:55:18 +0300

    Добавлено решение домашней задачи 41

--- S6Task41HW/Program.cs
+++ S6Task41HW/Program.cs
@@ -2,3 +2,31 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+int[] CreateArrayManual(int size)
+{
+    int[] arr = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i+1}-й элемент массива : ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+int NumbersThanZero(int[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count ++;
+    }
+    return count;
+}
+
+Console.Write("Введите размер массива: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayManual(sizeArr);
+Console.WriteLine();
+int numbersThanZero = NumbersThanZero(array);
+Console.WriteLine($"Элементов больше нуля -> {numbersThanZero}");
