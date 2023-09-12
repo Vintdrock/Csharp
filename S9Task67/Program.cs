41f429d1cb88581fcaf6f2dcd26920484c99fc5d
Виталий Матюшенко <kuyask@yandex.ru>
2023-09-12 21:11:48 +0300

    Добавлено решение задачи 67

--- S9Task67/Program.cs
+++ S9Task67/Program.cs
@@ -3,3 +3,29 @@
 // 453 -> 12
 // 45 -> 9
 
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+void Validation(int num)
+{
+    while(num < 1)
+    {
+        num = Prompt("Неверно введено число. Введите целочисленное число: ");
+    }
+}
+
+int FindSumOfDigits(int num) //453  45  4  0
+ {
+    if (num == 0) return 0;
+    return num % 10 + FindSumOfDigits(num / 10); // Это стэк : 453  45  4
+    // 4%10 + 45%10 + 453%10 = 4 + 5 + 3 + 0 (из return) = 12
+ }
+
+int number = Prompt("Введите целочисленное число: ");
+Validation(number);
+
+ Console.WriteLine(FindSumOfDigits(number));
