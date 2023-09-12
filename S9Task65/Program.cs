e530f12f19c4b70009b5041183f3b40718d33449
Виталий Матюшенко <kuyask@yandex.ru>
2023-09-12 20:48:20 +0300

    Добавлено решение задачи 65

--- S9Task65/Program.cs
+++ S9Task65/Program.cs
@@ -3,3 +3,29 @@
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
 
+void RangeNaturalNumbers(int from, int to)
+{
+    if(from < to)
+    {
+        Console.Write($"{from} ");
+        RangeNaturalNumbers(from + 1, to);
+    }
+    else if(from > to)
+    {
+        Console.Write($"{from} ");
+        RangeNaturalNumbers(from - 1, to);
+    }
+    else
+    {
+        Console.Write($"{from}");
+    }
+    
+}
+
+Console.WriteLine("Введите диапазон натуральных чисел");
+Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+RangeNaturalNumbers(numberOne, numberTwo);
