7d7d7fc6efa56a5d700f6e4ce65cd3bf94c76607
Виталий Матюшенко <kuyask@yandex.ru>
2023-09-14 19:23:09 +0300

    Добавлено решение домашней задачи 54

--- S8Task54HW/Program.cs
+++ S8Task54HW/Program.cs
@@ -7,3 +7,55 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintArray(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 2}");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] SortRowsMatrix (int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for(int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+    return matrix;
+}
+int[,] matrix = CreateMatrixRndInt(5, 5, 0, 9);
+Console.WriteLine("Исходный массив: ");
+PrintArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив: ");
+int[,] sortMatrix = SortRowsMatrix(matrix);
+PrintArray(sortMatrix);
