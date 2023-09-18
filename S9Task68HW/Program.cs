bf4883cc451d97a8f44340a17649d08288ce425d
Виталий Матюшенко <kuyask@yandex.ru>
2023-09-18 22:30:23 +0300

    Добавлено решение домашней задачи 68

--- S9Task68HW/Program.cs
+++ S9Task68HW/Program.cs
@@ -2,3 +2,20 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+int ack (int n, int m)
+{
+    while (n != 0)
+    {
+        if (m == 0) m = 1;
+        else m = ack(n, m - 1);
+        n = n - 1;
+    }
+    return m + 1;
+}
+Console.WriteLine("Введите диапазон натуральных чисел");
+Console.Write("Введите M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int akkerman = ack(numberM, numberN);
+Console.WriteLine($"Функция Аккермана равна: {akkerman}");
