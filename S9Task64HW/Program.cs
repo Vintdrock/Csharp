a0dfcb3a244b6b9c339691ea4607b28af80bc027
Виталий Матюшенко <kuyask@yandex.ru>
2023-09-18 20:31:06 +0300

    Добавлено решение домашней задачи 64

--- S9Task64HW/Program.cs
+++ S9Task64HW/Program.cs
@@ -2,3 +2,20 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    System.Console.WriteLine("Некорректный ввод!");
+    return;
+}
+Console.WriteLine();
+NaturalNumbers(1, number);
+
+void NaturalNumbers(int num, int n)
+{
+    if(num > n) return;
+    NaturalNumbers (num + 1, n);
+    Console.Write($"{num} ");
+}
