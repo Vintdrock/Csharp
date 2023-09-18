fc02f4161d65afda7686c418ff77cb22ddce8efd
Виталий Матюшенко <kuyask@yandex.ru>
2023-09-18 22:06:32 +0300

    Добавлено решение домашней задачи 66

--- S9Task66HW/Program.cs
+++ S9Task66HW/Program.cs
@@ -2,3 +2,28 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+void SumNaturalNumbers(int from, int to, int sum)
+{
+    int temp = 0;
+    if (from > to) 
+    {
+        temp = from;
+        from = to;
+        to = temp;
+    }
+    sum = sum + from;
+    if(from >= to)
+    {
+        Console.Write($"Сумма элементов равна: {sum}");
+        return;
+    }
+     SumNaturalNumbers(from + 1, to, sum);
+}
+
+Console.WriteLine("Введите диапазон натуральных чисел");
+Console.Write("Введите M: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+SumNaturalNumbers(numberOne, numberTwo, 0);
